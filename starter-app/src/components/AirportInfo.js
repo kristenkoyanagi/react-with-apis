cc0a2624b46229d88938218fcd450192f1856416
Hannah <hvk5388@psu.edu>
2021-03-06 22:01:16 -0500

    fixed search functions

--- starter-app/src/components/AirportInfo.js
+++ starter-app/src/components/AirportInfo.js
@@ -1,17 +1,37 @@
 import React, { useState } from 'react';
 import './AirportInfo.css';
+import Places from './Places';
 
 function AirportInfo() { 
-    const [places,setplaces] = useState([])
+    const [places,setPlaces] = useState([])
     const [query,setQuery] = useState("")
+    const [showPlaces,setShowPlaces] = useState(false)
 
-    function handleSubmit(give){
-        give.preventDefault();
+    function handleSubmit(e){
+        e.preventDefault();
         async function getMyAPI() {
-            const requestOptions = {
-                
+            const reqOptions = {
+                method:'GET',
+                headers: {
+                    "x-rapidapi-key": '40759afcbcmsh33253c61ac2d377p14f791jsn068a8b3badfd',
+	                "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+	                "useQueryString": true
+                }
             }
+
+            const qString = {
+                "query": query
+            }
+            
+            let response = await fetch('https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?' + new URLSearchParams(qString), reqOptions)
+            response = await response.json()
+            setPlaces(response.Places)
+            console.log(response.Places)
         }
+
+        getMyAPI()
+        setQuery("")
+        setShowPlaces(true)
     }
 
     return <div className="airportinfo">
@@ -19,9 +39,10 @@ function AirportInfo() {
             <label htmlFor="queryInput">
                 Place Name:
             </label>
-            <input id="queryInput" value={query} onChange={(give) => setQuery(give.target.value)} required/>
+            <input id="queryInput" value={query} onChange={(e) => setQuery(e.target.value)} required/>
             <button className="search">Search</button>
         </form>
+        {showPlaces ? <Places places={places}></Places> : <></>}
     </div>
 }
 
--- starter-app/src/components/Places.js
+++ starter-app/src/components/Places.js
@@ -2,7 +2,34 @@ import React from 'react';
 import './Places.css';
 
 function Places(props) { 
-    return;
+    return(
+        <div className="places">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Airport ID</th>
+                        <th>Airport Name</th>
+                        <th>Country ID</th>
+                        <th>Region ID</th>
+                        <th>City ID</th>
+                        <th>Country Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.places.map(place => {
+                        return (<tr>
+                            <th>{place.PlaceId}</th>
+                            <th>{place.PlaceName}</th>
+                            <th>{place.CountryId}</th>
+                            <th>{place.RegionId}</th>
+                            <th>{place.CityId}</th>
+                            <th>{place.CountryName}</th>
+                        </tr>)
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Places;
